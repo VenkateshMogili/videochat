1c6b3bd423b7884e554f83eb8f4652e993e026d8
VenkateshMogili <mogilivenkatesh3@gmail.com>
2020-07-06 02:51:31 +0530

    make it better

--- credentials.js
+++ credentials.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 module.exports = {
-  // key: null,
-  // cert: null
-  key: fs.readFileSync('privkey.pem'),
-  cert: fs.readFileSync('fullchain.pem')
+  key: null,
+  cert: null
+  // key: fs.readFileSync('privkey.pem'),
+  // cert: fs.readFileSync('fullchain.pem')
 };
